5dcbad112e3c09ebecb580a05ada450f8252876f
Artur Tarassow <t.artur@posteo.de>
2022-06-02 18:36:20 +0200

    add initial set of unit-tests; fix typos

---
+++ run_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+DIR=$(dirname $(realpath "$0")) 	# locate folder where this sh-script is located in
+
+PROJECT="ridge"
+
+SCRIPT1="./tests/run_tests.inp"
+
+cd $DIR
+echo "Switched to ${DIR}"
+
+
+gretlcli -b -e -q ${SCRIPT1}
+if [ $? -eq 0 ]
+then
+  echo "Success: Tests for script ${SCRIPT1} passed."
+  exit 0
+else
+  echo "Failure: Tests for script ${SCRIPT1} not passed."
+  exit 1
+fi
+
